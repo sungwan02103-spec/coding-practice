ab2dca2f815f41f76dfea57c149f859ccced64ec
sungwan02103-spec <sungwan02103@gmail.com>
2026-08-25 22:52:04 +0900

    [Passed] 알파벳에 따른 평가 (46ms, 16MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,15 @@
+grade = input()
+
+if grade == "S":
+    print("Superior")
+elif grade == "A":
+    print("Excellent")
+elif grade == "B":
+    print("Good")
+elif grade == "C":
+    print("Usually")
+elif grade == "D":
+    print("Effort")
+else:
+    print("Failure")
+    
